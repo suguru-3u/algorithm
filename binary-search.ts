b73cb1f3439cf8605acf52a10541a32d0fda7ab6
suguru-3u <suguru.13579515@gmail.com>
2022-08-06 17:17:36 +0900

    first commit

---
+++ binary-search.ts
@@ -0,0 +1,45 @@
+// 二分探索法
+// あらかじめ、昇順か降順に整列されているデータが対象
+// 探索範囲を半分に絞りながら探索を進める
+
+// １真ん中の要素を選ぶ処理
+// ２真ん中のデータと比較データがあっている処理
+// ３探索の範囲を半分に狭める処理
+// 目的のデータがない場合の処理も書く
+export {};
+
+function binarySearchMethod(serachData, targetData) {
+  let head = 0;
+  let tail = serachData.length;
+  let serachFlg = false;
+
+  while (head <= tail) {
+    let center = Math.round((head + tail) / 2);
+    console.log(`比較数字,${serachData[center]}`);
+    if (targetData === serachData[center]) {
+      serachFlg = true;
+      break;
+    }
+    if (targetData > serachData[center]) {
+      head = center + 1;
+    } else {
+      tail = center - 1;
+    }
+  }
+  searchDecision(serachFlg);
+}
+
+function searchDecision(flg) {
+  if (flg) {
+    console.log(`データが見つかりました`);
+  } else {
+    console.log(`データは見つかりませんでした`);
+  }
+}
+
+const serachData = [
+  11, 13, 15, 16, 17, 20, 21, 111, 123, 189, 201, 222, 989, 1000, 1001,
+];
+const targetData = 1000;
+
+binarySearchMethod(serachData, targetData);
---
+++ linear-search-method.ts
@@ -0,0 +1,17 @@
+// 線形探索法
+// 先頭から対象のプログラムがないか確認する
+// 単純でわかりやすいが、探索効率は良くない
+
+const serachData = [1, 2, 3, 4, 5];
+const targetData = 8;
+const serachDataLength = serachData.length;
+
+for (let i = 0; i < serachDataLength; i++) {
+  if (targetData === serachData[i]) {
+    console.log(`${i + 1}番目にありました！`);
+    break;
+  }
+  if (i === serachDataLength - 1) {
+    console.log("見つかりませんでした");
+  }
+}
